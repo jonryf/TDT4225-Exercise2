de44b65efca5742cc26785a189f56b4725dd6eee
annaher <31725804+annaher@users.noreply.github.com>
2020-10-02 10:43:13 +0200

    create solution to ex4 in part2

--- queries.py
+++ queries.py
@@ -14,3 +14,12 @@ class Queries:
             rows = self.cursor.fetchall()
             nr = rows[0][0]
             print("There are {} entries in the {} table".format(nr, table))
+
+    def q4(self):
+        query = "SELECT DISTINCT user_id FROM Activity " \
+                "WHERE transportation_mode='taxi'"
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        rows = [entry[0] for entry in rows]
+        print("The following users have taken a taxi:", rows)
+
